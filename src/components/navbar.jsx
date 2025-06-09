c75cbce128974dd5e71ef2349de17edb627bb87b
ehsanmim <115899665+ehsanmim@users.noreply.github.com>
2025-06-09 12:34:21 +0200

    Refactors navbar component for improved user experience

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import { ShareIcon } from "@heroicons/react/24/outline";
+import { BookmarkSquareIcon, ShareIcon } from "@heroicons/react/24/outline";
 import { Button, Navbar } from "react-daisyui";
 
 export default function index() {
@@ -16,11 +16,33 @@ export default function index() {
   return (
     <Navbar className="bg-base-100 shadow-sm">
       <div className="flex-1">
-        <Button tag="a" className="text-xl normal-case" color="ghost">
+        <Button
+          variant="link"
+          className="text-xl no-underline hover:no-underline!"
+          onClick={() => {
+            if (
+              window.confirm(
+                "Are you sure you want to leave this page? Unsaved data may be lost."
+              )
+            ) {
+              window.location.href = window.location.pathname;
+            }
+          }}
+        >
           Tailwind Grid Generator
         </Button>
       </div>
       <div className="flex-none">
+        <Button
+          tag="a"
+          color="info"
+          variant="outline"
+          onClick={() => alert("Simply bookmark this page!")}
+          className="mr-2"
+        >
+          <BookmarkSquareIcon className="h-6 w-6 mr-2" />
+          Save
+        </Button>
         <Button tag="a" shape="square" color="ghost" onClick={handleShare}>
           <ShareIcon className="h-6 w-6" />
         </Button>
